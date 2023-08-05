06c2082f3abbae691e8febfcf0bbea24cb1f1f96
daviddpacheco <rm551462@fiap.com.br>
2023-08-04 22:16:44 -0300

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function mudar(){
+    let novo= document.getElementById("idNome")
+    document.getElementById("titulo").requerid=true;
+    document.getElementById("titulo").innerHTML = novo.value;
+}
